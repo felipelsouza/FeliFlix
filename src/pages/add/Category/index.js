e8bacd2dd4af11d76a34de1fbe7914c28bc0a152
Felipe <felipelsouza@unipam.edu.br>
2020-07-29 22:29:34 -0300

    Form

--- src/pages/add/Category/index.js
+++ src/pages/add/Category/index.js
@@ -1,20 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import DefaultPage from '../../../components/DefaultPage';
+import FormField from '../../../components/FormField';
 
-import { Link } from 'react-router-dom'
-import DefaultPage from '../../../components/DefaultPage'
+function AddCategory() {
+    const initialValue = {
+        name: '',
+        description: '',
+        color: '',
+    }
+    const [category, setCategory] = useState([]);
+    const [values, setValues] = useState(initialValue);
 
-function addCategory() {
+
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value
+        })
+    }
+
+    function handleChange(e) {
+        setValue(
+            e.target.getAttribute('name'),
+            e.target.value
+        )
+    }
+
     return (
         <DefaultPage>
-            <h1>New Category</h1>
+            <h1>Add Category: {values.name}</h1>
 
-            <form>
-                <label>
-                    Name:
-                    <input type="text" />
-                </label>
-                <button>Add</button>
+            <form onSubmit={function handleSubmit(e) {
+                e.preventDefault();
+                setCategory([
+                    ...category,
+                    values
+                ]);
+
+                setValues(initialValue)
+            }}>
+
+                <FormField
+                label="Category Name:"
+                type="text"
+                name="name"
+                value={values.name}
+                onChange={handleChange}
+                />
+
+                <FormField
+                label="Description:"
+                type="textarea"
+                name="description"
+                value={values.description}
+                onChange={handleChange}
+                />
+
+                <FormField
+                label="color"
+                type="color"
+                name="color"
+                value={values.color}
+                onChange={handleChange}
+                />
+
+                <button>
+                    Add
+                </button>
             </form>
+            
+
+            <ul>
+                {category.map((category, index) => {
+                    return (
+                        <li key={`${category}${index}`}>
+                            {category.name}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Go to Home
@@ -23,4 +88,4 @@ function addCategory() {
     )
 }
 
-export default addCategory
+export default AddCategory;
